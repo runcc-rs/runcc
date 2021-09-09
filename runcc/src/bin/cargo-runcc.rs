3dc75f12e039cb1e5e2b3b413fa24ed279c00f55
EqualMa <equalma@outlook.com>
2021-09-10 01:02:57 +0800

    feat(cli): better exit message

--- runcc/src/bin/cargo-runcc.rs
+++ runcc/src/bin/cargo-runcc.rs
@@ -1,6 +1,18 @@
 use runcc::cli::run;
 
+struct ExitMessage(String);
+
+impl std::fmt::Debug for ExitMessage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    run().await
+async fn main() -> Result<(), ExitMessage> {
+    if let Err(err) = run().await {
+        Err(ExitMessage(format!("{}", err)))
+    } else {
+        Ok(())
+    }
 }
